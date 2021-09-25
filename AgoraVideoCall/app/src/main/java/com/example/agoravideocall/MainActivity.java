e7c9afad36a4a84d0c71a70f2a23bedf12333468
Mohid Rogers <mohidshaik588@gmail.com>
2021-09-25 19:58:18 +0530

    Sample 1-to-1 video call using latest agora sdk v4.0.0

---
+++ AgoraVideoCall/app/src/main/java/com/example/agoravideocall/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.agoravideocall;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+
+import android.os.Bundle;
+import io.agora.rtc2.Constants;
+import io.agora.rtc2.IRtcEngineEventHandler;
+import io.agora.rtc2.RtcEngine;
+import io.agora.rtc2.RtcEngineConfig;
+import io.agora.rtc2.video.VideoCanvas;
+import io.agora.rtc2.ChannelMediaOptions;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    // Fill the App ID of your project generated on Agora Console.
+    private String appId = "e657fa82ed7245b7a718ac7f5a6ff00f";
+    // Fill the channel name.
+    private String channelName = "video call";
+    // Fill the temp token generated on Agora Console.
+    private String token = "006e657fa82ed7245b7a718ac7f5a6ff00fIAAf79MNkm73R0nMOb42RvxTKvD3gpviiSYMvKP54hlymPwFm7AAAAAAEAAoMHytmW5PYQEAAQCVbk9h";
+    private void initializeAndJoinChannel() {
+        try {
+            RtcEngineConfig config = new RtcEngineConfig();
+            config.mContext = getBaseContext();
+            config.mAppId = appId;
+            config.mEventHandler = mRtcEventHandler;
+            mRtcEngine = RtcEngine.create(config);
+        } catch (Exception e) {
+            throw new RuntimeException("Check the error.");
+        }
+        // By default, video is disabled, and you need to call enableVideo to start a video stream.
+        mRtcEngine.enableVideo();
+        // Start local preview.
+        mRtcEngine.startPreview();
+
+        FrameLayout container = findViewById(R.id.local_video_view_container);
+        // Ceate a SurfaceView object and add it as a child to the FrameLayout.
+        SurfaceView surfaceView = new SurfaceView (getBaseContext());
+        container.addView(surfaceView);
+        // Pass the SurfaceView object to Agora so that it renders the local video.
+        mRtcEngine.setupLocalVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, 0));
+
+        ChannelMediaOptions options = new ChannelMediaOptions();
+        // Set both clients as the BROADCASTER.
+        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+        // For a video call scenario, set the channel profile as BROADCASTING.
+        options.channelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING;
+
+        // Join the channel with a temp token.
+        // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+        mRtcEngine.joinChannel(token, channelName, 0, options);
+    }
+    private void setupRemoteVideo(int uid) {
+        FrameLayout container = findViewById(R.id.remote_video_view_container);
+        SurfaceView surfaceView = new SurfaceView (getBaseContext());
+        surfaceView.setZOrderMediaOverlay(true);
+        container.addView(surfaceView);
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, uid));
+    }
+
+    private RtcEngine mRtcEngine;
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the remote host joining the channel to get the uid of the host.
+        public void onUserJoined(int uid, int elapsed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // Call setupRemoteVideo to set the remote video view after getting uid from the onUserJoined callback.
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+    };
+    private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA
+    };
+    private boolean checkSelfPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, requestCode);
+            return false;
+        }
+        return true;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // If all the permissions are granted, initialize the RtcEngine object and join a channel.
+        if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID)) {
+            initializeAndJoinChannel();
+        }
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        mRtcEngine.stopPreview();
+        mRtcEngine.leaveChannel();
+        RtcEngine.destroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
